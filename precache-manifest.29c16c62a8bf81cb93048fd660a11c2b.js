70aa6588ea8062398bf9f652c951219260e81d65
Ezema Nneka <85549384+nneky28@users.noreply.github.com>
2022-02-05 15:06:14 +0100

    Updates

---
+++ precache-manifest.29c16c62a8bf81cb93048fd660a11c2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8931e3902b0c5c813e2e9829aa0ed3f6",
+    "url": "_https://nneky28.github.io/Ecommerce_/index.html"
+  },
+  {
+    "revision": "6789af20139770042d27",
+    "url": "_https://nneky28.github.io/Ecommerce_/static/css/main.ba67c966.chunk.css"
+  },
+  {
+    "revision": "1cdec8008e7c009bcb66",
+    "url": "_https://nneky28.github.io/Ecommerce_/static/js/2.d6bbf151.chunk.js"
+  },
+  {
+    "revision": "00330752ade665012574",
+    "url": "_https://nneky28.github.io/Ecommerce_/static/js/3.9881a070.chunk.js"
+  },
+  {
+    "revision": "6789af20139770042d27",
+    "url": "_https://nneky28.github.io/Ecommerce_/static/js/main.c5f1b197.chunk.js"
+  },
+  {
+    "revision": "b803be9ae131a52cb491",
+    "url": "_https://nneky28.github.io/Ecommerce_/static/js/runtime~main.163538ba.js"
+  }
+]);
